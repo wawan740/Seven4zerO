4cccdd7df2ef9c9c631b3fbbfbf0270070060740
wawan740 <mpf.wawan740@gmail.com>
2017-12-24 17:27:57 +0700

    Update setgooglecloud.sh

--- setgooglecloud.sh
+++ setgooglecloud.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
-sudo su
-cd
-sudo passwd root
 sed -i 's/PermitRootLogin no/PermitRootLogin yes/g' /etc/ssh/sshd_config
 sed -i 's/PasswordAuthentication no/PasswordAuthentication yes/g' /etc/ssh/sshd_config
 reboot
